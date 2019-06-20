9fe7cb5cbffa97bf39d27980b87cb97be4fc6a47
Szymon Wiszczuk <golota60@gmail.com>
2019-06-21 00:17:27 +0200

    added post and delete to the api

--- routes/api/items.js
+++ routes/api/items.js
@@ -10,8 +10,31 @@ const Item = require('../../models/Item');
 router.get('/', (req, res) => {
   Item.find()
     .sort({ date: -1 }) //sortowanie datą desc
-    .then(items => res.json(items))
-    .catch(e => console.error(e));
+    .then(items => res.json(items));
 });
+
+// @route   POST api/items
+// @desc    add a post
+// @access Public // for now
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save()
+    .then(item => res.json(item));
+});
+
+// @route   DELETE api/items
+// @desc    delete a post
+// @access Public // for now
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success:false}))
+}); 
+
+
+
 
 module.exports = router;

5cf3d0823072b6198e560e3b2043dc56c37d7e15
Szymon Wiszczuk <golota60@gmail.com>
2019-06-21 15:39:22 +0200

    added react client via create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+
+// class App extends Component{
+// render(){
+//   return(
+//     <div className="App">
+//       <h1>siema</h1>
+//     </div>
+//   );
+
+// }
+
+//}
+
+export default App;

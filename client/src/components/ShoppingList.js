d7089064b04c60831868352bed54810cef43e449
Szymon Wiszczuk <golota60@gmail.com>
2019-06-24 17:17:22 +0200

    minor changes

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,20 +1,49 @@
 import React,{Component} from 'react';
 import {ListGroup,ListGroupItem,Button,Container} from 'reactstrap';
+import uuid from 'uuid';
 
 class ShoppingList extends Component{
+
+state={
+    items:[
+    {id:uuid(), name:'jeff'}
+
+]}
+
 render(){
+    const {items} = this.state;
     return(
         <Container>
-        <ListGroup>
-            <ListGroupItem>elo</ListGroupItem>
-            <ListGroupItem>siema</ListGroupItem>
-            <ListGroupItem>czes</ListGroupItem>
-            <ListGroupItem>elo2</ListGroupItem>
             <div>
             <Button
                 outline color = "primary"
+                onClick={() =>{
+                    const name = prompt("Add item");
+                    if(name){
+                        this.setState(state => ({
+                            items: [...state.items, {id: uuid(), name }]
+                        }));
+                    }
+                }}
                 >Add Item</Button>
                 </div>
+        <ListGroup>
+            {items.map(({id,name}) =>(
+                <ListGroupItem>
+                    <Button 
+                    className = 'remove-button'
+                    color = 'danger'
+                    size = 'sm'
+                    onClick={() => {
+                        this.setState(state => ({
+                            items: state.items.filter(item => item.id !== id)
+                        }));
+                    }}
+                    >&times;
+                    </Button>
+                    {name}
+                </ListGroupItem>    
+            ))}
         </ListGroup>
 
         </Container>
